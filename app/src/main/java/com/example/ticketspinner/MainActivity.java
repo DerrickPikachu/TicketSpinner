2fb6781578144fc5bdff9be4890e96e8dd5111b7
pikaChin <derrfgh4563@gmail.com>
2019-10-01 15:48:39 +0800

    Spinner function complete

--- app/src/main/java/com/example/ticketspinner/MainActivity.java
+++ app/src/main/java/com/example/ticketspinner/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.ticketspinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    class MainActivityListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            String[] cinemasName = getResources().getStringArray(R.array.cinemas);
+           result.setText("訂" + cinemasName[cinemas.getSelectedItemPosition()] + "的票");
+        }
+    }
+
+    Spinner cinemas;
+    TextView result;
+    Button bookBtn;
+    MainActivityListener listener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cinemas = findViewById(R.id.cinemasSpinner);
+        result = findViewById(R.id.resultText);
+        bookBtn = findViewById(R.id.bookBtn);
+        listener = new MainActivityListener();
+
+        bookBtn.setOnClickListener(listener);
     }
 }
